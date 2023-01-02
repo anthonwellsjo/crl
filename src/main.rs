9a696fd44455c0defefae9999388be68da578068
anthonwellsjo <anthonwellsjo@gmail.com>
2023-01-02 22:32:30 +0100

    chore: playing with tokio and daemonize

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
-fn main() {
-    println!("Hello, world!");
+// use arboard::Clipboard;
+use daemonize::Daemonize;
+use std::fs::File;
+use tokio::time;
+
+#[tokio::main]
+async fn main() {
+    // let mut clipboard = Clipboard::new().unwrap();
+    // println!("Clipboard text was: {}", clipboard.get_text().unwrap());
+
+    // let the_string = "Hello, world!";
+    // clipboard.set_text(the_string).unwrap();
+    // println!("But now the clipboard text should be: \"{}\"", the_string);
+
+    let stdout = File::create("/tmp/daemon.out").unwrap();
+    let stderr = File::create("/tmp/daemon.err").unwrap();
+
+    let daemonize = Daemonize::new()
+        .pid_file("/tmp/test.pid") // Every method except `new` and `start`
+        .chown_pid_file(true) // is optional, see `Daemonize` documentation
+        .working_directory("/tmp") // for default behaviour.
+        .user("nobody")
+        .group("daemon") // Group name
+        .group(2) // or group id.
+        .umask(0o777) // Set umask, `0o027` by default.
+        .stdout(stdout) // Redirect stdout to `/tmp/daemon.out`.
+        .stderr(stderr) // Redirect stderr to `/tmp/daemon.err`.
+        .exit_action(|| println!("Executed before master process exits"))
+        .privileged_action(|| "Executed before drop privileges");
+
+    match daemonize.start() {
+        Ok(_) => {
+            println!("Success, daemonized");
+            println!("hello");
+            let mut interval = time::interval(time::Duration::from_secs(2));
+            for _i in 0..5 {
+                interval.tick().await;
+                println!("hello");
+            }
+        }
+        Err(e) => eprintln!("Error, {}", e),
+    }
 }
